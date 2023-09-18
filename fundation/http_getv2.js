7d8619e5f8e790fa56cad504284448e60ff3e7da
wen <jwcastillo@gmail.com>
2023-09-18 11:52:41 -0300

    comentarios get v2

--- fundation/http_getv2.js
+++ fundation/http_getv2.js
@@ -1,31 +1,42 @@
-import http from 'k6/http';
-import {group, check, sleep} from 'k6';
-import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
-import exec from 'k6/execution';
+// Importando módulos necesarios para realizar pruebas con k6
+import http from 'k6/http';  // Proporciona funciones para hacer solicitudes HTTP
+import {group, check, sleep} from 'k6';  // Herramientas para estructurar y verificar las pruebas y pausar la ejecución
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';  // Función que genera un número entero aleatorio entre dos valores
+import exec from 'k6/execution';  // Proporciona información sobre la ejecución actual de la prueba
 
+// Definiendo la URL base que se usará en las solicitudes. Si no está definido en las variables de entorno, se usará 'https://test-api.k6.io/'
 const BASE_URL = __ENV.BASE_URL || 'https://test-api.k6.io/'
 
+// Función principal de la prueba
 export default function (){
-    let res={}
+    let res={}  // Inicializando una variable para almacenar respuestas de las solicitudes HTTP
+
+    // Grupo para la solicitud GET a la URL base
     group("GET V1", function(){
-        res = http.get(BASE_URL)
-        check(res,{"status is 200": (res) => res.status === 200})
-
+        res = http.get(BASE_URL)  // Haciendo la solicitud GET
+        check(res,{"status is 200": (res) => res.status === 200})  // Verificando que la respuesta tenga un estado HTTP de 200
     })
-    sleep(0.1)
+
+    sleep(0.1)  // Pausando la ejecución durante 0.1 segundos
+
+    // Grupo para obtener información sobre cocodrilos
     group("GET Cocodrile",function() {
-        res = http.get(`${BASE_URL}public/crocodiles/`);
-        check(res,{"status is 200": (res) => res.status === 200})
+        res = http.get(`${BASE_URL}public/crocodiles/`);  // Haciendo una solicitud GET para obtener cocodrilos
+        check(res,{"status is 200": (res) => res.status === 200})  // Verificando que la respuesta tenga un estado HTTP de 200
+        // Comentado: Console log de un ID de cocodrilo aleatorio
         //console.log(JSON.stringify(res.json()[randomIntBetween(0,res.json().length-1)].id))
-
     })
-    sleep(1)
-    
+
+    sleep(1)  // Pausando la ejecución durante 1 segundo
+
+    // Grupo para obtener información sobre un cocodrilo específico usando su ID
     group("GET Cocodrile ID", function(){
+        // Haciendo una solicitud GET para obtener información de un cocodrilo usando un ID aleatorio
         res = http.get(`${BASE_URL}public/crocodiles/${res.json()[randomIntBetween(0,res.json().length-1)].id}`);
-        check(res,{"status is 200": (res) => res.status === 200})
-
+        check(res,{"status is 200": (res) => res.status === 200})  // Verificando que la respuesta tenga un estado HTTP de 200
     })
+
+    // Imprimiendo información detallada sobre la ejecución actual de la prueba
     console.log(`Execution context
 
     Instance info
@@ -59,7 +70,6 @@ export default function (){
     VU ID in test: ${exec.vu.idInTest}
     VU tags: ${exec.vu.tags}`);
     
+    // Comentado: Console log de la respuesta
     // console.log(json.stringify(response))
-
-
 }
